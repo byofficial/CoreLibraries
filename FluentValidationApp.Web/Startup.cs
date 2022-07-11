360db042421663f5f54ab3e1c3e3980e172daf0e
Burak YILDIZ <burak@yaztem.com>
2022-07-11 23:45:42 +0300

    Add custom error handler fluent validation for customer

--- FluentValidationApp.Web/Startup.cs
+++ FluentValidationApp.Web/Startup.cs
@@ -2,15 +2,11 @@ using FluentValidation.AspNetCore;
 using FluentValidationApp.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FluentValidationApp.Web
 {
@@ -34,6 +30,11 @@ namespace FluentValidationApp.Web
             services.AddControllersWithViews().AddFluentValidation(options =>
             {
                 options.RegisterValidatorsFromAssemblyContaining<Startup>();
+            });
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
             });
         }
 
