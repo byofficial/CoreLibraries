29ce577bd7f36e8b0f009ca05617fe96e22f86f8
Burak YILDIZ <burak@yaztem.com>
2022-07-11 19:53:55 +0300

    add custom validator for birthday property

--- FluentValidationApp.Web/Controllers/CustomersController.cs
+++ FluentValidationApp.Web/Controllers/CustomersController.cs
@@ -53,7 +53,7 @@ namespace FluentValidationApp.Web.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Email,Age")] Customer customer)
+        public async Task<IActionResult> Create([Bind("Name,Email,Age,BirthDay")] Customer customer)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace FluentValidationApp.Web.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Age")] Customer customer)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Age,BirthDay")] Customer customer)
         {
             if (id != customer.Id)
             {
--- FluentValidationApp.Web/Models/Customer.cs
+++ FluentValidationApp.Web/Models/Customer.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FluentValidationApp.Web.Models
 {
@@ -11,6 +8,6 @@ namespace FluentValidationApp.Web.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public int Age { get; set; }
-
+        public DateTime? BirthDay { get; set; }
     }
 }
